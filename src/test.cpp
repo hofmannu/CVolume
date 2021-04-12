bed11c6ecce46d2eea112eec9089de54888991e1
Urs Hofmann <hofmannu@ethz.ch>
2021-04-12 10:54:00 +0200

    initial commit, no testing functions yet

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+/*
+	volume class
+	Author: Urs Hofmann
+	Mail: hofmannu@ethz.ch
+	Date: 12.04.2021
+
+	An object oriented approach to process volumetric datasets written in C++
+*/
+
+#include "volume.h"
+
+int main()
+{
+	volume testVol;
+	return 0;
+}
